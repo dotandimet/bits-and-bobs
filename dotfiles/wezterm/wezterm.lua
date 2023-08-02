dbf69a1c7c173b1f4b70e200977137cee4ef66d2
Dotan Dimet <dotan@corky.net>
2023-08-02 16:13:20 +0300

    move to wezterm, for true color vim themes

---
+++ dotfiles/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- config.font = wezterm.font 'Menlo'
+-- config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 16.0
+config.color_scheme = 'rose-pine'
+
+config.audible_bell = "Disabled"
+
+config.initial_rows = 30
+config.initial_cols = 120
+
+config.window_background_opacity = 0.9
+
+return config
+
+
+
