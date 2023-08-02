b7c60ec8dcd8450a11645c37cbee4e1916576f93
Dotan Dimet <dotan@corky.net>
2023-08-02 16:15:56 +0300

    adding filename patterns for syntax highlighting, fixing ^X^F to be more useful

--- dotfiles/nvim2/lua/dotan/set.lua
+++ dotfiles/nvim2/lua/dotan/set.lua
@@ -15,9 +15,20 @@ vim.opt.hlsearch = false
 -- vim.opt.incsearch = true
 vim.opt.scrolloff = 8
 
+local mySetSyntax = function(pattern, file_type, syntax)
+    vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"},
+    { pattern = pattern, callback = function ()
+        vim.opt.syntax = syntax
+        vim.opt.filetype = file_type
+    end })
+end
+
 -- au BufRead,BufNewFile *.vue set ft=html syntax=html
+mySetSyntax({"*.vue"}, "html", "html")
 -- au BufRead,BufNewFile Jenkinsfile set ft=groovy syntax=groovy
+mySetSyntax({"Jenkinsfile"}, "groovy", "groovy")
 -- au BufRead,BufNewFile Rprofile set ft=R syntax=R
+mySetSyntax({"Rprofile"}, "R", "R")
 -- 
 -- set hidden
 -- let g:ackprg = 'rg --vimgrep --ignore-file=.gitignore'
@@ -25,7 +36,7 @@ vim.opt.scrolloff = 8
 -- vim.opt.foldmethod     = 'expr'
 -- vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
 ---WORKAROUND
-vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
+vim.api.nvim_create_autocmd({"BufEnter","BufAdd","BufNew","BufNewFile","BufWinEnter"}, {
   group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
   callback = function()
     vim.opt.foldmethod     = 'expr'
@@ -34,3 +45,25 @@ vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEn
   end
 })
 ---ENDWORKAROUND
+
+-- make ^X^F more useful, by looking in the directory of the current open file
+-- from: https://superuser.com/questions/604122/vim-file-name-completion-relative-to-current-file
+-- :autocmd InsertEnter * let save_cwd = getcwd() | set autochdir
+-- :autocmd InsertLeave * set noautochdir | execute 'cd' fnameescape(save_cwd)
+
+SaveCwd = vim.fn.getcwd()
+
+vim.api.nvim_create_autocmd({"InsertEnter"}, {
+    callback = function()
+        SaveCwd = vim.fn.getcwd()
+        vim.opt.autochdir = true
+    end
+})
+
+vim.api.nvim_create_autocmd({"InsertLeave"}, {
+    callback = function()
+        vim.opt.autochdir = false
+        vim.api.nvim_set_current_dir(SaveCwd)
+    end
+})
+
